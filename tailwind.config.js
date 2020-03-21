9418de081f6702fd0a425068cc16ce045b7cef1a
masterthepixel <45719243+masterthepixel@users.noreply.github.com>
2020-03-21 16:39:10 -0400

    BlendMode, DebugScreens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,45 @@
 module.exports = {
   theme: {
-    extend: {},
+    interFontFeatures: {
+      default: ['calt', 'liga', 'kern'],
+      numeric: ['tnum', 'salt', 'ss02']
+    },
+    extend: {
+      fontSize: {
+        'xs': '11px',
+        's': '13px',
+        'm': '17px',
+        'l': '28px',
+        'xl': '36px',
+        '2xl': '48px',
+        '3xl': '64px',
+        base: '16px',
+      },
+      lineHeight: {
+        'lg': '1.6',
+        'md': '1.33',
+        'sm': '1.25',
+        'xs': '1',
+      },
+    }
   },
-  variants: {},
+  variants: { // all the following default to ['responsive']
+    mixBlendMode: ['responsive'],
+    backgroundBlendMode: ['responsive'],
+    isolation: ['responsive'],
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    textColor: ['responsive', 'hover', 'focus', 'visited', 'group-hover'],
+    borderWidth: ['responsive', 'last', 'hover', 'focus'],
+    borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
+  },
+
   plugins: [
     require('@tailwindcss/ui'),
+    require('tailwindcss-blend-mode')(), // no options to configure https://github.com/hacknug/tailwindcss-blend-mode
+    require('tailwindcss-debug-screens'),
+    require('tailwindcss-font-inter')(),
+    require('@tailwindcss/ui')({
+      layout: 'sidebar',
+    })
   ]
 }
